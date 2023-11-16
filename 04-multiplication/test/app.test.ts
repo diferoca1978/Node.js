7265b4e5d616713039226a0220f01303754efed3
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2023-11-16 12:54:17 -0500

    All done 04-multiplication

--- 04-multiplication/test/app.test.ts
+++ 04-multiplication/test/app.test.ts
@@ -1,5 +1,31 @@
-describe('app.ts', () => {
-  test('should be true', () => {
-    expect(true).toBe(true);
+import { serverApp } from '../src/presentation/server.app';
+
+describe('Test app.ts', () => {
+  test('should call server.run with default values', async () => {
+    const serverAppMock = jest.fn();
+    serverApp.run = serverAppMock;
+
+    process.argv = [
+      'node',
+      'app.ts',
+      '-b',
+      '10',
+      '-l',
+      '20',
+      '-s',
+      '-n',
+      'test-file',
+      '-d',
+      'custom-output',
+    ];
+    await import('../src/app');
+
+    expect(serverAppMock).toHaveBeenCalledWith({
+      base: 10,
+      limit: 20,
+      displayTable: true,
+      fileName: 'test-file',
+      fileDestination: 'custom-output',
+    });
   });
 });
--- 04-multiplication/test/server.app.test.ts
+++ 04-multiplication/test/server.app.test.ts
@@ -20,37 +20,51 @@ describe('Server app', () => {
 
   test('should run serverApp with options', () => {
     //$ Integration test
-    // const logSpy = jest.spyOn(console, 'log'); //! When we use jest.spyOn() function without mockImplementation, we are only creating a function that listen that log.
-    // const createTableSpy = jest.spyOn(CreateTable.prototype, 'execute');
-    // const saveFileSpy = jest.spyOn(SaveFile.prototype, 'execute');
-    // serverApp.run(options);
-    // expect(logSpy).toHaveBeenCalledTimes(2);
-    // expect(logSpy).toHaveBeenCalledWith('Server running...');
-    // expect(logSpy).toHaveBeenLastCalledWith('File created 🆗');
-    // expect(createTableSpy).toHaveBeenCalledTimes(1);
-    // expect(createTableSpy).toHaveBeenCalledWith({
-    //   base: options.base,
-    //   limit: options.limit,
-    // });
-    // expect(saveFileSpy).toHaveBeenCalledTimes(1);
-    // expect(saveFileSpy).toHaveBeenCalledWith({
-    //   fileContent: expect.any(String),
-    //   fileDetination: options.fileDestination,
-    //   fileName: options.fileName,
-    // });
+    const logSpy = jest.spyOn(console, 'log'); //! When we use jest.spyOn() function without mockImplementation, we are only creating a function that listen that log.
+    const createTableSpy = jest.spyOn(CreateTable.prototype, 'execute');
+    const saveFileSpy = jest.spyOn(SaveFile.prototype, 'execute');
+    serverApp.run(options);
+    expect(logSpy).toHaveBeenCalledTimes(2);
+    expect(logSpy).toHaveBeenCalledWith('Server running...');
+    expect(logSpy).toHaveBeenLastCalledWith('File created 🆗');
+    expect(createTableSpy).toHaveBeenCalledTimes(1);
+    expect(createTableSpy).toHaveBeenCalledWith({
+      base: options.base,
+      limit: options.limit,
+    });
+    expect(saveFileSpy).toHaveBeenCalledTimes(1);
+    expect(saveFileSpy).toHaveBeenCalledWith({
+      fileContent: expect.any(String),
+      fileDetination: options.fileDestination,
+      fileName: options.fileName,
+    });
   });
 
   test('Should run with custom values mocked', () => {
     //$ Unitary test
 
     const logMock = jest.fn();
-    const createMock = jest.fn();
-    const saveFileMock = jest.fn();
+    const logErrorMock = jest.fn();
+    const createMock = jest.fn().mockReturnValue('2 * 1 = 2');
+    const saveFileMock = jest.fn().mockReturnValue(true);
 
     console.log = logMock;
+    console.error = logErrorMock;
     CreateTable.prototype.execute = createMock;
     SaveFile.prototype.execute = saveFileMock;
 
     serverApp.run(options);
+
+    expect(logMock).toHaveBeenCalledWith('Server running...');
+    expect(createMock).toHaveBeenCalledWith({
+      base: options.base,
+      limit: options.limit,
+    });
+    expect(saveFileMock).toHaveBeenCalledWith({
+      fileContent: '2 * 1 = 2',
+      fileDestination: options.fileDestination,
+      fileName: options.fileName,
+    });
+    expect(logErrorMock).not.toHaveBeenCalledWith();
   });
 });
